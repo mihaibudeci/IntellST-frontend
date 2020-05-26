2ba9ca835d86ea0f416db8014cfc53cdd7ed0d9c
aignatiuc <arina.ignatiuc@ee.utm.md>
2020-05-26 06:03:13 +0300

    Fix bug with css import :bug:

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,5 @@
 module.exports = {
   "transpileDependencies": [
     "vuetify"
-  ],
-  css: {
-    loaderOptions: {
-      sass: {
-        prependData: `@import "@/styles/_variables.scss";`
-      }
-    }
-  }
+  ]
 };
