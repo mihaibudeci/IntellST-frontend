feef3ffd7075bac54472d7ed0b3caef89dd88859
aignatiuc <arina.ignatiuc@ee.utm.md>
2020-05-26 05:59:31 +0300

    Integrate dahsboard template :sparkles:

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,66 @@
 <template>
-  <v-app>
+  <v-app id="inspire">
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+      clipped
+    >
+      <v-list dense>
+        <v-list-item link to="/">
+          <v-list-item-action>
+            <v-icon>mdi-view-dashboard</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Dashboard</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link  to="/about">
+          <v-list-item-action>
+            <v-icon>mdi-information</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>About</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar
+      app
+      color="grey darken-3"
+      clipped-left
+    >
+          <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <img :src="require('./assets/logo.svg')" class="mt-2 mr-3" height="100"/>
+    </v-app-bar>
 
     <v-content>
-      <router-view></router-view>
+      <v-container
+        class="fill-height"
+        fluid
+      >
+    <router-view></router-view>
+      </v-container>
     </v-content>
 
+    <v-footer app padless>
+      <v-card-text class="py-2 white--text text-center">
+        Copyright &copy; {{ new Date().getFullYear() }} <strong>IntellST.</strong> All Rights Reserved
+      </v-card-text>
+    </v-footer>
   </v-app>
 </template>
 
 <script>
-
-export default {
-  name: 'App',
-
-  data: () => ({
-    //
-  }),
-};
+  export default {
+    props: {
+      source: String,
+    },
+    data: () => ({
+      drawer: false,
+    }),
+    created () {
+      this.$vuetify.theme.dark = true
+    },
+  }
 </script>
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,12 @@
 module.exports = {
   "transpileDependencies": [
     "vuetify"
-  ]
-}
+  ],
+  css: {
+    loaderOptions: {
+      sass: {
+        prependData: `@import "@/styles/_variables.scss";`
+      }
+    }
+  }
+};
