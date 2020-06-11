e0c21f02b67f492fcc1c3dabc068ddef0ed45b01
aignatiuc <arina.ignatiuc@ee.utm.md>
2020-06-11 15:52:43 +0300

    Charts integration

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify';
+import './plugins/chartist'
 
 Vue.config.productionTip = false
 
@@ -10,3 +11,4 @@ new Vue({
   vuetify,
   render: h => h(App)
 }).$mount('#app')
+
