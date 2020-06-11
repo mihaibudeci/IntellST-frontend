4c481ce71e5710a35ae820b387dd9a223c237ad2
Mihai <mihaibudeci@gmail.com>
2020-06-11 19:41:45 +0300

    starting validation

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -8,11 +8,19 @@
       <v-list-item-content>
         <div class="row" style="height: 70px;">
           <div class="col-8">
-            <v-list-item-subtitle class="mt-3">Set maximum allowed temperature C</v-list-item-subtitle>
+            <v-list-item-subtitle class="mt-3">Set maximum allowed temperature C°</v-list-item-subtitle>
           </div>
             <v-col cols="3">
               <v-text-field
+                      type="number"
+                      min="35"
+                      max="38"
+                      v-validate="{ required: true }"
+                      step=".1"
+                      size="2"
                       label=""
+                      suffix="°"
+                      value="35.0"
                       outlined
                       dense
               ></v-text-field>
@@ -24,6 +32,9 @@
           </div>
           <v-col cols="3">
             <v-text-field
+                    type="number"
+                    required
+                    size="2"
                     label=""
                     outlined
                     dense
@@ -40,3 +51,4 @@
     </v-card-actions>
   </v-card>
 </template>
+
